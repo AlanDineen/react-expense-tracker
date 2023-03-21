1543f0b2e98f7d6ba95043cedca28e7e6785f7e3
Alan <adineenphoto@gmail.com>
2023-03-21 23:08:50 +0000

    ADR-expenses-tracker-first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Expenses from "./components/Expenses";
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 765.98,
+      date: new Date(2022, 3, 30)
+    },
+    {
+      id: 'e2',
+      title: 'Car Insurance2',
+      amount: 265.98,
+      date: new Date(2022, 2, 13)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance3',
+      amount: 565.98,
+      date: new Date(2022, 4, 26)
+    },
+    {
+      id: 'e4',
+      title: 'Car Insurance4',
+      amount: 665.98,
+      date: new Date(2022, 6, 26)
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Expenses expenses={expenses}></Expenses>
     </div>
   );
 }
